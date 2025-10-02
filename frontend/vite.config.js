6948afc517eb686ac2d2b24961208576c0499719
mpho-moloto <molotoofficial@gmail.com>
2025-10-02 10:41:01 +0200

    Hopefully this works

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   server: {
-    port: 3000,  // Set default port
+    port: 3000,
     host: true,
-    open: true   // Auto-open browser
+    open: true
   }
 })
